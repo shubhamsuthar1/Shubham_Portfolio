cdef1fa289c54422404d6a27423371e2356ce5e4
Shubham Suthar <140694207+shubhamsuthar1@users.noreply.github.com>
2024-06-28 12:19:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend Development"],
+    strings: ["Frontend Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
